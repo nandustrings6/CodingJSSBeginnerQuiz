c62f67b37049ec3de063004957abe6fdc5e3e9a9
unknown <nandustrings6@gmail.com>
2023-08-27 00:48:40 -0700

    updates v2

--- javascript.js
+++ javascript.js
@@ -1,16 +1,41 @@
 const StartQuiz = document.querySelector('.StartQuiz');
 
-function quizQuestions() {
-    const question1 = {
-         question: "The condition if an if / else statement is enclosed with_____________.",
-         answer1: "Quotes",
-         answer2: "Curly Brackets",
-         answer3: "Parenthsis",
-         answer4: "Square Brackets",
-         correctAnswer: "Curly Brackets"
-        };
-    
-        
-        
-    if ()
-}
+const Questions = [
+    {
+        number: 1,
+        question: "The condition if an if / else statement is enclosed with_____________.",
+        answers: [
+            {Text: "Quotes", correct: false},
+            {Text: "Curly Brackets", correct: true},
+            {Text:"Parenthesis", correct: false},
+            {Text: "Square Brackets", correct: false},
+        ]
+
+    },
+    {
+        number: 2,
+        question: "Commonly used data types DO Not include:The condition if an if / else statement is enclosed with_____________.",
+        Options: ["Strings","Booleans","Alerts","Numbers"]
+
+    },
+    {
+        number: 3,
+        question: "Arrays in JavaScript can be used to store_____________.",
+        Options: ["Number and strings","Other arrays","Booleans","All the above"]
+
+    },
+    {
+        number: 4,
+        question: "String values must be enclosed within __________ when being assinged to variables.",
+        Options: ["Commas","Curly brackets","Quotes","Parenthsis"]
+
+    },
+    {
+        number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is",
+        Options: ["JavaScript","Terminal/Bash","For loops","Console.log"]
+
+    },
+];
+
+StartQuiz.addEventListener("Click", quizQuestions);
