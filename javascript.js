97e3ee6a5525e4cf6ebfb33864d69ade6db5df0e
unknown <nandustrings6@gmail.com>
2023-08-27 00:14:57 -0700

    updated html, started js, added high score html

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const StartQuiz = document.querySelector('.StartQuiz');
+
+function quizQuestions() {
+    const question1 = {
+         question: "The condition if an if / else statement is enclosed with_____________.",
+         answer1: "Quotes",
+         answer2: "Curly Brackets",
+         answer3: "Parenthsis",
+         answer4: "Square Brackets",
+         correctAnswer: "Curly Brackets"
+        };
+    
+        
+        
+    if ()
+}
