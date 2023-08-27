b91fb530125a67ac1e0ea2aa6aedaaa82f28af58
unknown <nandustrings6@gmail.com>
2023-08-27 13:46:48 -0700

    js edits with questions & functions

--- javascript.js
+++ javascript.js
@@ -1,9 +1,137 @@
-const StartQuiz = document.querySelector('.StartQuiz');
+const startQuiz = document.querySelector('.StartQuiz');
 const timer = 90; 
-const timerID; 
+var timercountdown;
+const timerIdEl = document.getElementById('Timer');
+const nextBtnEl = document.getElementById("nextbtn");
+const questionAnswersEl = document.getElementById("questionanswers");
+const page1 = document.querySelector(".Page1");
+const questionEl = document.getElementById("question");
+const answersEl = document.getElementById("answers");
+const displayAnswer = document.querySelector('.displayanswer');
+const viewHighScores = document.querySelector('.Viewhighscores'); //re-check this later
+const initialSubmitBtn = document.querySelector('.initialsubmitbtn');
+const clearBtn = document.querySelector('.clearbtn');
+const initialSubmitEl = document.getElementById('initialssubmit'); 
+const restartBtnEl = document.getElementById('restartbtn'); 
+const yourScoreEl = document.getElementById('yourscore'); //re-check this later
+var viewHighScoresOnPage = JSON.parse(localStorage.getItem("listhighscoreslocalstorage")) || []; //re-check this later
+var questionSlides; 
 
+//DONE - Timer 
 
-const Questions = [
+function countDownFunction () {
+    timer--;
+    timerIdEl.textContent = "Time" + timer; 
+    if (timer <= 0) {
+        score();
+    }
+};
+
+//DONE - sequence of events - click on StartQuizbtn event listener
+
+startQuiz.addEventListener("Click", startQuestionSlides);
+nextbtn.addEventListener("Click", () => {
+    questionSlides++
+    giveNextQuestion()
+});
+
+//DONE - To startQuestionSlides function
+
+function startQuestionSlides () {
+    timerCountDown = setInterval(countDownFunction, 1000);
+    page1.classList.add("hidepage");
+    questionSlides = questions
+    questionAnswersEl.classList.remove("hidepage");
+
+    countDownFunction();
+    giveNextQuestion();
+};
+
+//DONE - To go to next question giveNextQuestion function 
+
+function giveNextQuestion () {
+    resetNextQuestion();
+    showNextQuestion([questionSlides]);
+};
+
+//DONE - Start displaying questions showNextQuestion function
+
+function showNextQuestion (questionDisplay) {
+    questionEl.innerText = questionDisplay.question
+    questionDisplay.answer.forEach(answer => {
+        var button = document.createElement ("button")
+        button.innerText = answer.text
+        button.classList.add("answerbtn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answersEl.appendChild(button)
+        
+    });
+};
+
+//Done - resetNextQuestion function
+
+function resetNextQuestion () {
+    nextBtnEl.classList.add("hidepage")
+    displayAnswer.classList.add("hidepage")
+    while (answersEl.firstChild) {
+        answersEl.removeChild
+        (answersEl.firstChild)
+    }
+};
+
+//DONE - function selectAnswer to check if answer is correct or wrong, display message to user
+
+    // not using this as dont want to change colors with wrong answer 
+    //Array.from(answersEl.children).forEach (button => {
+    //    setStatusClass(button, button.dataset.correct)})
+    //not using shuffled question option
+    //not creating -  function setStatusClass to show correct answer by changing button color
+
+function selectAnswer(e) {
+    var selectedButton = e.target; 
+    var correct = selectedButton.dataset.correct;
+    displayAnswer.classList.remove("hidepage")
+    if (correct) {
+        displayAnswer.innerHTML = "That's correct!";
+    }
+    else {
+        displayAnswer.innerHTML = "Incorrect answer!";
+        if (timer <= 10) {
+            timer = 0; 
+        } else {
+            timer -= 10; 
+        }
+    }
+    
+    if (questionSlides > 0) {
+        nextBtnEl.classList.remove("higepage")
+        displayAnswer.classList.remove("hidepage")}
+    else {
+        startQuiz.classList.remove("hidepage")
+        score();
+    }
+};
+
+//Done - function 'score' to compute score based on time left and display to the user
+
+function score () {
+    clearInterval(timercountdown); 
+    timerIdEl.textContent = "Time:" + timer;
+    setTimeout(function () {
+        questionAnswersEl.classList.add("hidepage");
+        document.getElementById("name&scorepage").classList.remove("higepage");
+        document.getElementById("yourscore").textContent = "Your final score is" + timer;
+    }, 2000)
+}; 
+
+//code line 155 to 233 to be written// 
+
+//Questions at the bottom to avoid confusion 
+
+const questions = [
     {
         number: 1,
         question: "The condition if an if / else statement is enclosed with_____________.",
@@ -17,28 +145,46 @@ const Questions = [
     },
     {
         number: 2,
-        question: "Commonly used data types DO Not include:The condition if an if / else statement is enclosed with_____________.",
-        Options: ["Strings","Booleans","Alerts","Numbers"]
+        question: "Commonly used data types DO Not include:",
+        answers: [
+            {Text: "Strings", correct: false},
+            {Text: "Booleans", correct: false},
+            {Text:"Alerts", correct: true},
+            {Text: "Numbers", correct: false},
+        ]
 
     },
     {
         number: 3,
         question: "Arrays in JavaScript can be used to store_____________.",
-        Options: ["Number and strings","Other arrays","Booleans","All the above"]
+        answers: [
+            {Text: "Number and strings", correct: false},
+            {Text: "Other arrays", correct: false},
+            {Text:"Booleans", correct: false},
+            {Text: "All the above", correct: true},
+        ]
 
     },
     {
         number: 4,
         question: "String values must be enclosed within __________ when being assinged to variables.",
-        Options: ["Commas","Curly brackets","Quotes","Parenthsis"]
+        answers: [
+            {Text: "Commas", correct: false},
+            {Text: "Curly brackets", correct: false},
+            {Text:"Quotes", correct: true},
+            {Text: "Parenthsis", correct: false},
+        ]
 
     },
     {
         number: 5,
         question: "A very useful tool used during development and debugging for printing content to the debugger is",
-        Options: ["JavaScript","Terminal/Bash","For loops","Console.log"]
+        answers: [
+            {Text: "JavaScript", correct: false},
+            {Text: "Terminal/Bash", correct: false},
+            {Text:"For loops", correct: false},
+            {Text: "Console.log", correct: true},
+        ]
 
     },
 ];
-
-StartQuiz.addEventListener("Click", quizQuestions);
